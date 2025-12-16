c570be4d9946e50ef950f192b530834bd972a79c
Chris Loumeau <c2loumeau@gmail.com>
2025-12-15 21:58:13 -0500

    Added anagram

--- Hashmap/Anagram.rs
+++ Hashmap/Anagram.rs
@@ -1,1 +1,26 @@
-x
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len() {
+            return false;
+        }
+        
+        let mut char_count: HashMap<char, i32> = HashMap::new();
+        
+        // Count characters in s
+        for c in s.chars() {
+            *char_count.entry(c).or_insert(0) += 1;
+        }
+        
+        // Subtract characters in t
+        for c in t.chars() {
+            *char_count.entry(c).or_insert(0) -= 1;
+            if char_count[&c] < 0 {
+                return false;
+            }
+        }
+        
+        true
+    }
+}

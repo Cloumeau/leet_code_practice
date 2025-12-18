53440dcc704755f4d2580e6e50d06f646601163d
Chris Loumeau <c2loumeau@gmail.com>
2025-12-18 17:20:51 -0500

    Added

--- Stack/valid_parentheses.py
+++ Stack/valid_parentheses.py
@@ -18,7 +18,7 @@ class Solution:
         }
         for char in s:
             if char in closing_to_opening:
-                if not in stack or stack[-1] !+= closing_to_opening[char]:
+                if not stack or stack[-1] != closing_to_opening[char]:
                     return False
                 stack.pop()
             else:
